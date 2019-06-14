79530e4d1fdddc474c4d3eeb920cb65199a0b4a4
Andrew Wen <Wen.Andrew@mayo.edu>
2019-06-14 17:39:08 -0500

    API Expansion, try to fetch other repository first as a submodule

--- src/main/java/edu/mayo/dhs/ievaluate/api/IEvaluate.java
+++ src/main/java/edu/mayo/dhs/ievaluate/api/IEvaluate.java
@@ -1,15 +1,33 @@
 package edu.mayo.dhs.ievaluate.api;
 
 import edu.mayo.dhs.ievaluate.api.models.applications.ProfiledApplication;
+import edu.mayo.dhs.ievaluate.api.plugins.IEvaluatePlugin;
 import edu.mayo.dhs.ievaluate.api.util.StorageProvider;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.UUID;
 
 /**
  * Global API entry point for the IEvaluate Framework
  */
 public interface IEvaluate {
+
+    /* ===== Begin Plugin Management Functions ===== */
+
+    /**
+     * @param name  The name of the plugin
+     * @param clazz The plugin class that is expected
+     * @param <T>   The {@link IEvaluatePlugin} implementation type corresponding to clazz
+     * @return The instantiated instance of the {@link IEvaluatePlugin}
+     */
+    <T extends IEvaluatePlugin> T getPlugin(String name, Class<T> clazz);
+
+    /**
+     * Do not call before application initialization is complete
+     * @return A mapping of Registered Plugin Name => Plugin Instances
+     */
+    Map<String, IEvaluatePlugin> getRegisteredPlugins();
 
     /* ===== Begin Application Management Functions ===== */
 
@@ -19,7 +37,7 @@ public interface IEvaluate {
     Collection<ProfiledApplication> getRegisteredApplications();
 
     /**
-     * @param uid The unique ID of the application
+     * @param uid   The unique ID of the application
      * @return The application with this unique identifier
      */
     ProfiledApplication getApplication(UUID uid);
